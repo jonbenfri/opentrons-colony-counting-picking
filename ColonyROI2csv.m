9dfab3b1ad22204e5a87b8582859145f6d00f1e7
Jonathan Friedman <jonbenfri@gmail.com>
2019-05-11 19:35:12 -0400

    adds octave script to convert colony ROI binary dataset to csv

---
+++ ColonyROI2csv.m
@@ -0,0 +1,14 @@
+% Octave script to convert ImageJ ROI dataset from binary to JSON format
+% Note: Octave uses 1-indexing (indices are from 1 to N)
+
+ROI_FILE = 'photos/600px-E.coliAgarpicture.jpg-RoiSet.zip';
+DATA_DIR = 'data';
+
+% `ReadImageJROI.m` function file is assumed to be in working directory
+
+[csvROIs] = ReadImageJROI('photos/600px-E.coliAgarpicture.jpg-RoiSet.zip');
+
+for (i = csvROIs)
+    colony_str = char(i(1){1}.strName);
+    csvwrite([DATA_DIR "/" colony_str ".csv"], i(1){1}.mnCoordinates);
+end;
