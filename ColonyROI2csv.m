bd625e30e6591a67f62c445e26c33439b74ba853
Jonathan Friedman <jonbenfri@gmail.com>
2019-05-11 21:07:18 -0400

    moves away from hardcoded variables and filesystem separator

--- ColonyROI2csv.m
+++ ColonyROI2csv.m
@@ -1,12 +1,13 @@
 % Octave script to convert ImageJ ROI dataset from binary to JSON format
 % Note: Octave uses 1-indexing (indices are from 1 to N)
 
-ROI_FILE = 'photos/600px-E.coliAgarpicture.jpg-RoiSet.zip';
+ROI_SET_DIR = 'photos';
+ROI_FILE = '600px-E.coliAgarpicture.jpg-RoiSet.zip';
 DATA_DIR = 'data';
 
 % `ReadImageJROI.m` function file is assumed to be in working directory
 
-[csvROIs] = ReadImageJROI('photos/600px-E.coliAgarpicture.jpg-RoiSet.zip');
+[csvROIs] = ReadImageJROI([ROI_SET_DIR filesep() ROI_FILE]);
 
 for (i = csvROIs)
     colony_str = char(i(1){1}.strName);
