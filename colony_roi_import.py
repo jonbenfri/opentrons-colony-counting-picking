dea602d8a28f6ebf4febc327e6387b998648f66f
Jonathan Friedman <jonbenfri@gmail.com>
2019-05-11 20:57:28 -0400

    adds center of mass method for specified colony

--- colony_roi_import.py
+++ colony_roi_import.py
@@ -7,6 +7,7 @@ import numpy as np
 
 # Change working directory to data directory
 DATA_DIR = "data"
+START_DIR = os.getcwd()
 
 
 class ColonyROIImportCSV(object):
@@ -19,11 +20,17 @@ class ColonyROIImportCSV(object):
         csv_files_list = glob("*.csv")
         self.csv_files_list = csv_files_list
 
-    def colony_count(self):
-        # Get a colony count
-        colony_count = len(self.csv_files_list)
-        self.colony_count = colony_count
-        return self.colony_count
+        # Go back to starting directory
+        os.chdir(START_DIR)
+
+    def get_colony_count(self):
+        try:
+            return self.colony_count
+        except AttributeError:
+            # Get a colony count
+            colony_count = len(self.csv_files_list)
+            self.colony_count = colony_count
+            return self.colony_count
 
     def colony_data(self):
         try:
@@ -31,6 +38,7 @@ class ColonyROIImportCSV(object):
         except AttributeError:
             colonies = []
 
+            os.chdir(self.data_dir)
             for csv_filename in self.csv_files_list:
                 colony_name = os.path.splitext(csv_filename)[0]
                 roi_data = np.genfromtxt(csv_filename, delimiter=',') 
@@ -40,4 +48,37 @@ class ColonyROIImportCSV(object):
                         'roi_data': roi_data,
                     }
                 colonies.append(colony_data)
+
+            os.chdir(START_DIR)
+            self.colonies = colonies
             return colonies
+
+    def center_of_mass(self, colony_num=0):
+        """
+        Returns (x,y) coordinate of specified colony center of mass
+        """
+
+        try:
+            self.colonies
+        except AttributeError:
+            print("Run `ColonyROIImportCSV.colony_data` first to import data.")
+
+        N = self.get_colony_count()
+        if colony_num not in range(N):
+            raise ValueError("Pick a colony number from 0 to " + str(N))
+
+        x_ = 0
+        y_ = 0
+
+        colony_roi = self.colonies[colony_num]
+        roi_data = colony_roi['roi_data']
+        roi_pixel_count = len(roi_data)
+
+        for coord in roi_data:
+            x_ = x_ + coord[0]
+            y_ = y_ + coord[1]
+
+        x_ = x_/roi_pixel_count
+        y_ = y_/roi_pixel_count
+
+        return x_, y_
--- tests.py
+++ tests.py
@@ -10,12 +10,20 @@ class TestColonyROIImportCSV(unittest.TestCase):
 
     def test_colony_count(self):
         roi_import = ColonyROIImportCSV(data_dir=DATA_DIR)
-        colony_count = roi_import.colony_count()
+        colony_count = roi_import.get_colony_count()
 
         roi_data = roi_import.colony_data()
 
         self.assertEqual(colony_count, 16)
         self.assertEqual(len(roi_data), 16)
+
+    def test_center_of_mass(self):
+        roi_import = ColonyROIImportCSV(data_dir=DATA_DIR)
+        colony_count = roi_import.get_colony_count()
+
+        colony_roi_data = roi_import.colony_data()
+        print(roi_import.center_of_mass(1))
+
 
 if __name__ == "__main__":
     unittest.main()
