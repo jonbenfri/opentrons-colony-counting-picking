8ead2ce39fa2c7e4c76a496ac912b28f1d1a9d38
Jonathan Friedman <jonbenfri@gmail.com>
2019-05-11 20:26:24 -0400

    adds import for colony ROI data

--- agar_plate.py
+++ agar_plate.py
@@ -5,6 +5,24 @@
 # 2. Create labware to interact with
 
 from opentrons import labware, instruments
+from colony_roi_import import ColonyROIImportCSV
+
+DATA_DIR = "data"
+PLATE_SLOT = 1
+
+
+######################
+# Colony Data Import #
+######################
+
+# Import colony location data
+roi_import = ColonyROIImportCSV(data_dir=DATA_DIR)
+roi_data = roi_import.colony_data()
+
+
+###############
+# Robot Setup #
+###############
 
 # Declare pipette
 try:
@@ -12,7 +30,14 @@ try:
 except:
     pass
 
-# Declare labware
+
+# Declare agar_plate labware
+# Strategy 1: Align plate(s) to grid or do 1x1 well plate,
+#    use vector in move_to relative to center of dish
+# Strategy 2: Each pixel is a well, create `colony` method
+#    to access plate colonies, e.g. `plate.colony('1')`
+
+# Strategy 1:
 plate_name = '75mm_agar_plate'
 if plate_name not in labware.list():
     custom_plate = labware.create(
@@ -23,12 +48,11 @@ if plate_name not in labware.list():
             depth=10, # random value
             volume=200) # random value
 
+# Load labware
 try:
-    plate = labware.load(plate_name, '3')
+    plate = labware.load(plate_name, PLATE_SLOT)
 except:
     pass
 
 # Do a test transfer
 p300.transfer(10, plate.wells('A1'), plate.wells('A1'))
-
-# Import colony location data
